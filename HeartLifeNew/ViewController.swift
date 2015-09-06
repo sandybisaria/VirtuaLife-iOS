29d09ca5ca40cbc4e7093336f99b92589322b1f9
Syed Raziq Mohideen <mohideen.syed@rocketmail.com>
2015-09-06 06:26:48 -0400

    Initial Commit

---
+++ HeartLifeNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartLifeNew
+//
+//  Created by Syed Mohideen on 9/6/15.
+//  Copyright © 2015 VirtuaLife. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
